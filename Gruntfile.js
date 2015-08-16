cdc46cb447d395fe3245fe74a4a390b570c0f718
Dragos Tiselice <dragostiselice@gmail.com>
2015-08-16 14:38:15 +0300

    Added assets copying to grunt.

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,13 +5,38 @@ module.exports = function(grunt) {
         options: {},
         files: {
           'dist/index.html': 'src/index.html',
-          'dist/blog.html': 'src/blog.html',
-          'dist/post.html': 'src/post.html',
-          'dist/author.html': 'src/author.html'
+          'dist/home.hbs': 'src/home.hbs',
+          'dist/index.hbs': 'src/index.hbs',
+          'dist/post.hbs': 'src/post.hbs',
+          'dist/author.hbs': 'src/author.hbs'
         },
       },
+    },
+
+    copy: {
+      assets: {
+        expand: true,
+        cwd: 'src/assets/',
+        src: '**',
+        dest: 'dist/assets/'
+      },
+      package: {
+        expand: true,
+        cwd: 'src/',
+        src: 'package.json',
+        dest: 'dist/'
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-vulcanize');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+
+  grunt.registerTask('generate', function() {
+    grunt.task.run(
+      'vulcanize',
+      'copy:assets',
+      'copy:package'
+    );
+  });
 }
